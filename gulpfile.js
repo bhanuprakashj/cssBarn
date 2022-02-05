bea9a0500c348e9895bcb2b89afefa689accc3ba
bhanuprakashj <bhanu.prakash.4545@gmail.com>
2022-02-05 18:39:50 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint-new');
+const w3cjs = require('gulp-w3cjs');
+const changeContent = require('gulp-change');
+const htmlmin = require('gulp-htmlmin');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+// const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const minifyCss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const cssReplaceUrl = require('gulp-css-url-adjuster');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+
+// clears dist folder
+gulp.task('clean', () => del('dist/**'));
+
+// copy assets from dist
+gulp.task('copyAssets', () => gulp.src('src/assets/**').pipe(gulp.dest('dist/assets/')));
+
+// process scss file main.scss
+gulp.task('processSass', () => gulp.src('src/scss/main.scss')
+	.pipe(sourcemaps.init({ loadMaps: true }))
+	.pipe(sass().on('error', sass.logError))
+	.pipe(cssReplaceUrl({
+		replace: ['../../assets', '../assets'],
+	}))
+	.pipe(rename('style.css'))
+	.pipe(minifyCss())
+	.pipe(rename('style.min.css'))
+	.pipe(sourcemaps.write('maps'))
+	.pipe(gulp.dest('dist/css')));
+
+// starts server
+gulp.task('browserSync', () => browserSync.init({
+	server: {
+		baseDir: './dist',
+	},
+}));
+
+// lints js files imported in index.js
+gulp.task('lintJs', () => gulp.src('./src/js/*.js')
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError())
+	.pipe(babel())
+	.pipe(gulp.dest('./dist/js'))
+	.pipe(uglify())
+	.pipe(rename('index.min.js'))
+	.pipe(gulp.dest('./dist/js/')));
+
+// lints html files
+gulp.task('lintHtml', () => gulp.src('./src/**/*.html')
+	.pipe(w3cjs())
+	.pipe(w3cjs.reporter())
+	.pipe(changeContent((content) => content.replaceAll('../dist', '.')))
+	.pipe(htmlmin({ collapseWhitespace: true }))
+	.pipe(gulp.dest('./dist/')));
+
+// task to watch html/css/js files and reload browser
+gulp.task('watchingFiles', () => {
+	gulp.watch('src/scss/**/*.scss', gulp.series('processSass')).on('change', browserSync.reload);
+	gulp.watch('src/**/*.html', gulp.series('lintHtml')).on('change', browserSync.reload);
+	gulp.watch('src/js/**/*.js', gulp.series('lintJs')).on('change', browserSync.reload);
+});
+
+// dev task
+gulp.task('dev', gulp.series('clean', 'copyAssets', 'processSass', 'lintHtml', 'lintJs', gulp.parallel('browserSync', 'watchingFiles')));
+// build task
+gulp.task('build', gulp.series('clean', 'copyAssets', 'processSass', 'lintHtml', 'lintJs'));
