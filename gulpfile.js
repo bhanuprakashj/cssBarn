ea8cf2c323a427f00299b8aab46132baf247062a
bhanuprakashj <bhanu.prakash.4545@gmail.com>
2022-02-15 10:04:26 +0530

    tweaked setup for segrating docs related assets/css/js

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,16 @@ const concat = require('gulp-concat');
 const cssReplaceUrl = require('gulp-css-url-adjuster');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const mergeStream = require('merge-stream');
 
 // clears dist folder
 gulp.task('clean', () => del('dist/**'));
 
-// copy assets from dist
-gulp.task('copyAssets', () => gulp.src('src/assets/**').pipe(gulp.dest('dist/assets/')));
+// copy assets to dist
+gulp.task('copyAssets', () => mergeStream(
+	gulp.src('src/assets/**').pipe(gulp.dest('dist/assets/')),
+	gulp.src('src/docsAssets/**').pipe(gulp.dest('dist/docsAssets/')),
+));
 
 // process scss file main.scss
 gulp.task('processSass', () => gulp.src('src/scss/main.scss')
@@ -48,6 +52,19 @@ gulp.task('processCss', () => gulp.src('src/css/main.css')
 	.pipe(sourcemaps.write('maps'))
 	.pipe(gulp.dest('dist/css')));
 
+// process docs css file main.css
+gulp.task('processDocsCss', () => gulp.src('src/docsCss/main.css')
+	.pipe(sourcemaps.init({ loadMaps: true }))
+	.pipe(cssReplaceUrl({
+		replace: ['../../docsAssets', '../docsAssets'],
+	}))
+	.pipe(rename('docsStyle.css'))
+	.pipe(autoprefixer())
+	.pipe(minifyCss())
+	.pipe(rename('docsStyle.min.css'))
+	.pipe(sourcemaps.write('maps'))
+	.pipe(gulp.dest('dist/docsCss')));
+
 // starts server
 gulp.task('browserSync', () => browserSync.init({
 	server: {
@@ -67,6 +84,18 @@ gulp.task('lintJs', () => gulp.src('./src/js/*.js')
 	.pipe(sourcemaps.write('maps'))
 	.pipe(gulp.dest('./dist/js/')));
 
+// lints docs js files imported in index.js
+gulp.task('lintDocsJs', () => gulp.src('./src/docsJs/*.js')
+	.pipe(sourcemaps.init({ loadMaps: true }))
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError())
+	.pipe(browserify())
+	.pipe(uglify())
+	.pipe(concat('docsJsBundle.min.js'))
+	.pipe(sourcemaps.write('maps'))
+	.pipe(gulp.dest('./dist/docsJs/')));
+
 // lints html files
 gulp.task('lintHtml', () => gulp.src('./src/**/*.html')
 	.pipe(w3cjs())
@@ -79,11 +108,12 @@ gulp.task('lintHtml', () => gulp.src('./src/**/*.html')
 gulp.task('watchingFiles', () => {
 	// gulp.watch('src/scss/**/*.scss', gulp.series('processSass')).on('change', browserSync.reload);
 	gulp.watch('src/css/**/*.css', gulp.series('processCss')).on('change', browserSync.reload);
+	gulp.watch('src/docsCss/**/*.css', gulp.series('processDocsCss')).on('change', browserSync.reload);
 	gulp.watch('src/**/*.html', gulp.series('lintHtml')).on('change', browserSync.reload);
 	gulp.watch('src/js/**/*.js', gulp.series('lintJs')).on('change', browserSync.reload);
 });
 
 // dev task
-gulp.task('dev', gulp.series('clean', 'copyAssets', 'processCss', 'lintHtml', 'lintJs', gulp.parallel('browserSync', 'watchingFiles')));
+gulp.task('dev', gulp.series('clean', 'copyAssets', 'processDocsCss', 'processCss', 'lintHtml', 'lintDocsJs', 'lintJs', gulp.parallel('browserSync', 'watchingFiles')));
 // build task
-gulp.task('build', gulp.series('clean', 'copyAssets', 'processCss', 'lintHtml', 'lintJs'));
+gulp.task('build', gulp.series('clean', 'copyAssets', 'processDocsCss', 'processCss', 'lintHtml', 'lintDocsJs', 'lintJs'));
